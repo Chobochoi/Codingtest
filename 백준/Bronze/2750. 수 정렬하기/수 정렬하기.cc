ec9e035c21d9b57128c9fc5b7a76848603791eea
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-07-29 08:44:32 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using std::vector;
+
+int main()
+{
+	// YAL 강의 후 필수로 추가하는 코드 3줄
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> v;
+	int n = 0;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int input = 0;
+		cin >> input;
+
+		v.push_back(input);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i] << '\n';
+	}
+
+	return 0;
+}
