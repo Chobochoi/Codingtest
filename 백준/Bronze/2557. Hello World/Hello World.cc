5775884db50cab3574f4e334406cc6a1f00579e9
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-08-09 23:48:46 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#define endl '\n';
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);    
+
+    cout << "Hello World!";
+}
+
