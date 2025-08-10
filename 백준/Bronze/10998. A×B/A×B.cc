8c192ed59cf4aa8441fd4a44528e0a66b3cb75af
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-08-10 22:10:33 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#define endl '\n';
+
+using namespace std;
+
+int N, M;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);    
+
+    cin >> N >> M;
+    
+    cout << N * M;
+}
+
