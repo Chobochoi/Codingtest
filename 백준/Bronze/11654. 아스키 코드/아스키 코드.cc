261c2b51db6f41499cc8abf04726bbd4e7b5dff7
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-08-20 08:20:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <stack>
+#define endl "\n";
+
+using namespace std;
+
+char c;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);          
+
+    cin >> c;
+
+    cout << int(c);
+}
+
