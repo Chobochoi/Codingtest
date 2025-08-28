30f1cbc2937507588e7ab27d22cf8ae928b766ea
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-08-28 15:44:44 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 110964 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+plate = []
+count = []
+
+for _ in range(M):
+    plate.append(input())
+
+for a in range(M-7):
+    for b in range(N-7):
+        W = 0
+        B = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if plate[i][j] != 'W':
+                        W += 1
+                    if plate[i][j] != 'B':
+                        B += 1
+                else:
+                    if plate[i][j] != 'B':
+                        W += 1
+                    if plate[i][j] != 'W':
+                        B += 1
+        count.append(min(W, B))
+
+print(min(count))
