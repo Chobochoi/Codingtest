cc498c5d29da2651d388ca57985808a851519d71
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-11 11:04:04 +0900

    [Silver III] Title: 분수 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+C, D = map(int, input().split())
+deno = 0
+num = 0
+
+if B < D:
+    Min = B
+    Max = D
+else:
+    Min = D
+    Max = B
+while Max % Min != 0:
+    temp = Max % Min
+    Max = Min
+    Min = temp
+
+deno = Min * (B//Min) * (D//Min) #분모
+num = A * (D//Min) + C * (B//Min) #분자
+
+newNum = num
+newDeno = deno
+result = 1
+
+#한번 더 최대공약수 구해서 나누기 (기약분수)
+while newDeno % newNum != 0: 
+    result = newDeno % newNum
+    newDeno = newNum
+    newNum = result
+    
+print(int(num/newNum), int(deno/newNum))
