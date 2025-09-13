b4d449853d857811cc8ab6f0f45cc851bb941f03
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-14 07:57:19 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+FROM ANIMAL_INS AS A 
+JOIN ANIMAL_OUTS AS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE LIKE 'Intact%' and B.SEX_UPON_OUTCOME NOT LIKE 'Intact%'
+ORDER BY A.ANIMAL_ID
+
+
+
+-- Spayed Female 이미 중성화
+-- Intac Male 들어온 후 중성화
+-- Intac Female 들어온 후 중성화
