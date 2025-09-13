c0e2689359c845bb25fb8be97eb749ba3c86f5f0
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-13 19:33:31 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.46 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def solution(A,B):
+        
+    A.sort()
+    B.sort(reverse = True)
+    
+    result = 0
+    
+    for i in range(len(A)):
+        result += A[i] * B[i]
+    
+    return result
+    
+    
+                
+                
