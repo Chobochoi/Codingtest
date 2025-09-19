44f136464748acf7498d61a9f67add1b7eb1f6a7
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-19 09:47:40 +0900

    [Silver II] Title: 나무 자르기, Time: 432 ms, Memory: 258360 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+K = list(map(int, input().split()))
+
+
+start = 0
+end = max(K)
+
+result = 0
+
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+            
+    for x in K:
+        if x > mid:
+            total += x - mid
+        
+    if total < M:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+        
+print(result)
+        
