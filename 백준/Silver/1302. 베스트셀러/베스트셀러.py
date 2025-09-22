f8581f892c455136c21476d9a7a233e4505c34b9
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-22 14:33:33 +0900

    [Silver IV] Title: 베스트셀러, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,20 @@
+N = int(input())
+dic = {}
+
+for i in range(N):
+    name = input()
+    if name not in dic:
+        dic[name] = 1
+    else:
+        dic[name] += 1
+        
+max_value = max(dic.values())
+
+st = []
+
+for key, value in dic.items():
+    if value == max_value:
+        st.append(key)
+        
+st = sorted(st)
+print(st[0])
