015b3849186d94c4fe5edc0d80c87af52ccb0778
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-23 23:17:28 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,20 +1,21 @@
 N = int(input())
 
-for i in range(N):
+for _ in range(N):
     M = int(input())
-    
+
     dic = {}
-    
-    for i in range(M):
+
+    for _ in range(M):
         name, kind = map(str, input().split())
         
         if kind in dic:
             dic[kind] += [name]
         else:
-            dic[kind] = [name]
-            
+            dic[kind] = [name]  
+    
     answer = 1
-    for value in dic:
-        answer *= (len(dic[value])+ 1)
-        
-    print(answer-1)
+
+    for value in dic:      
+        answer *= len(dic[value]) + 1
+
+    print(answer-1)       
