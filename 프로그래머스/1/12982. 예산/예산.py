6bd4cd0162af518fcd8e12fa6896c1105f25616e
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2025-09-29 09:36:34 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,16 @@
+def solution(d, budget):
+    
+    d.sort()
+    
+    answer = 0
+    
+    for cost in d:
+        if budget >= cost:
+            budget -= cost
+            answer += 1
+        else:
+            break
+            
+    return answer
+    
+    
