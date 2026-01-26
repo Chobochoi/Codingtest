fbe917844a336753747e2b460e15078c32808c00
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-01-27 08:48:37 +0900

    [Silver I] Title: 절댓값 힙, Time: 228 ms, Memory: 45640 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,24 @@
+import sys
+from queue import PriorityQueue
+
+print = sys.stdout.write
+input = sys.stdin.readline
+
+N = int(input())
+
+myQueue = PriorityQueue()
+
+for i in range(N):
+    request = int(input())
+    
+    if request == 0:
+        if myQueue.empty():
+            print('0\n')
+        else:
+            temp = myQueue.get()
+            print(str((temp[1]))+'\n')
+            
+    else:
+        myQueue.put((abs(request), request))
+
+            
