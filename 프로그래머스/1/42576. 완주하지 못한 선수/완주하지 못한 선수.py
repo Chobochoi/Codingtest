4c378335de7f13993d6b37d827b3fccf70873374
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-02-23 14:06:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.81 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,18 @@
 def solution(participant, completion):
+    hash_dict = {}
     
-    participant.sort()
-    completion.sort()
-    
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
+    # 1. 참가자 명단 기록 (이름: 등장 횟수)
+    for p in participant:
+        if p in hash_dict:
+            hash_dict[p] += 1
+        else:
+            hash_dict[p] = 1
+            
+    # 2. 완주자 명단 차감
+    for c in completion:
+        hash_dict[c] -= 1
         
-    
-    return participant[-1]
-        
-    
-    
-        
-    
-    
+    # 3. 완주하지 못한 선수 찾기
+    for key, value in hash_dict.items():
+        if value == 1:
+            return key
