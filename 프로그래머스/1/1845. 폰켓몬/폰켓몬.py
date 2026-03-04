6a7f38493714a252adecb92db7e958d8c233ac00
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-03-04 09:47:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,18 +1,16 @@
-from itertools import combinations
-
+# 260304
+# 해당 문제의 가장 중요한 요소 집합(Set)
+# Set은 중복을 제거할 때 사용하는 자료구조
 def solution(nums):
     
-    st = []
+    max_select = len(nums) // 2
     
-    pick = len(nums)/2
-        
-    for num in nums:
-        if num not in st:
-            st.append(num)            
-    st.sort()
+    kind_total = len(set(nums))
     
-    if len(st) >= pick:
-        return pick
+    if max_select >= kind_total :
+        return kind_total
     else:
-        return len(st)
+        return max_select
+    
+    
             
