72c005fb7387a36526170bcb2d3cfcfdcf3e430b
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-03-10 08:35:53 +0900

    [Silver II] Title: 나무 자르기, Time: 432 ms, Memory: 258360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,28 @@
-# 260310
-# 프로그래머스 유사 문제
 import sys
-
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
 K = list(map(int, input().split()))
+
 
 start = 0
 end = max(K)
-    
+
+result = 0
+
 while start <= end:
-    mid = (start + end) // 2    
-
-    all_total = 0
-    
-    for tree in K:
-        if tree > mid:
-            all_total += tree - mid
-
-    if all_total >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
+    total = 0
+    mid = (start + end) // 2
+            
+    for x in K:
+        if x > mid:
+            total += x - mid
         
-print(end)
+    if total < M:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+        
+print(result)
+        
