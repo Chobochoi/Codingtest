506e05ddf07e7cc3a3118d68f1aa5e1b945c0f1f
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-03-20 08:37:48 +0900

    [level 2] Title: 전화번호 목록, Time: 120.48 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,9 +2,8 @@ def solution(phone_book):
     
     phone_book.sort()
     
-    for start, value in zip(phone_book, phone_book[1:]):
-        if value.startswith(start):
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
             return False
         
     return True
-        
