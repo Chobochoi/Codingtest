af20296224f2936c2650f3934f1e7637e891d5dd
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-03-23 13:29:40 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,22 @@
+#260323
 def solution(clothes):
     
     dic = {}
-    
+        
     for name, kind in clothes:
-        if kind in dic.keys():
-            dic[kind] += [name]
+        if kind in dic:
+            dic[kind] += 1
         else:
-            dic[kind] = [name]
+            dic[kind] = 1
             
     answer = 1
     
-    for _, value in dic.items():
-        answer *= (len(value) + 1)        
+    for num in dic.values():
+        answer *= (num + 1)
+        
     return answer - 1
+    
+    
+            
+    
+        
