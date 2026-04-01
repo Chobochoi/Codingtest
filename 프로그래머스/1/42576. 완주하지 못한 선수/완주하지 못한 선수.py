214240db8cb5f5f407e8d0f2423163659e1cd7b9
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-04-01 22:13:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.40 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,7 @@
-# 260302
-# Python 내재함수 Counter 활용.
+# 260401
 import collections
 
 def solution(participant, completion):
-    # 1. 참가자와 완주자의 빈도수 차이를 계산하여 완주하지 못한 선수(1명)를 남김
     answer = collections.Counter(participant) - collections.Counter(completion)
-    
-    # 2. 남은 1명의 데이터에서 Key값만 추출하여 리스트로 변환 후 문자열 반환
+       
     return list(answer.keys())[0]
