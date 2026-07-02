a529e3e8b99d7a57543e333f5c4036c243b342db
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-07-02 09:04:11 +0900

    [level 1] Title: 최소직사각형, Time: 0.80 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,9 +1,10 @@
 def solution(sizes):
-    w = []
-    h = []
+    max_width = 0
+    max_height = 0
     
-    for card in sizes:
-        w.append(max(card))
-        h.append(min(card))
+    for w, h in sizes:
+        # 두 변 중 긴 쪽을 width, 짧은 쪽을 height로 간주하여 최대값 갱신
+        max_width = max(max_width, max(w, h))
+        max_height = max(max_height, min(w, h))
         
-    return max(w) * max(h)
+    return max_width * max_height
